9a87f828ac2c6215eb07233be64801f707e91b19
Miroslav Repický <127839739+miroslavrepicky@users.noreply.github.com>
2026-04-19 22:41:32 +0200

    Delete projectiles directory

--- projectiles/StatusEffectFactory.java
+++
@@ -1,9 +0,0 @@
-package sk.stuba.fiit.projectiles;
-
-import sk.stuba.fiit.attacks.StatusEffect;
-import sk.stuba.fiit.characters.Character;
-
-@FunctionalInterface
-public interface StatusEffectFactory {
-    StatusEffect create(Character target);
-}
