3eb0068edfdf10cba059ad50536e8e79dbf49e14
Miroslav Repický <127839739+miroslavrepicky@users.noreply.github.com>
2026-04-22 23:32:03 +0200

    Add files via upload

--- core/exceptions/SaveException.java
+++ core/exceptions/SaveException.java
@@ -4,7 +4,7 @@ package sk.stuba.fiit.core.exceptions;
  * Výnimka pri zlyhaní uloženia hry.
  * Rozlišuje chyby ukladania od ostatných herných chýb.
  */
-public final class SaveException extends ShadowQuestException {
+public class SaveException extends ShadowQuestException {
     public SaveException(String message, Throwable cause) {
         super(message, cause);
     }
